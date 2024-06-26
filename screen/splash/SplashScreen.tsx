d89c3ff482cb64da53440218f5ea4258e14d8769
himawan.masyaid <himawan.masyaid@gmail.com>
2024-06-27 00:42:34 +0700

    create : delay 2 second to splash screen

--- screen/splash/SplashScreen.tsx
+++ screen/splash/SplashScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer, NavigationProp, useNavigation } from "@react-navigation/native";
 import {
   Button,
@@ -12,25 +12,51 @@ import {
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "navigation/RootStackParamList";
 
-const SplashScreen = ({navigation}: NativeStackScreenProps<RootStackParamList>) => {
+const SplashScreen = () => {
+// const SplashScreen = ({navigation}: NativeStackScreenProps<RootStackParamList>) => {
 
-  return (
-    <View style={styles.container}>
-      <Pressable onPress={() => navigation.navigate('main')}>
+    const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 seconds delay
+  })
+
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
         <Image
           style={{ width: 100, height: 100 }}
-          source={require("../../assets/images/ic_react.png")}
+          source={require('../../assets/images/ic_react.png')}
         />
-      </Pressable>
-    </View>
-  );
+      </View>
+    );
+  } else {
+    navigation.navigate('main');
+    return null;
+  }
+
+  // return (
+  //   <View style={styles.container}>
+  //     <Pressable onPress={() => navigation.navigate('main')}>
+  //       <Image
+  //         style={{ width: 100, height: 100 }}
+  //         source={require("../../assets/images/ic_react.png")}
+  //       />
+  //     </Pressable>
+  //   </View>
+  // );
 };
 
 export default SplashScreen;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
     flex: 1,
     alignItems: "center", // horizontal
     justifyContent: "center", // vertical
